5c4d0d450c2f0400209a7962f0157d746bf1f9ea
carollyb <carolly.barbosa@gmail.com>
2022-07-23 19:03:36 -0300

    :sparkles: sets up app  context and global provider

--- src/context/GlobalProvider.tsx
+++ src/context/GlobalProvider.tsx
@@ -3,8 +3,38 @@ import GlobalContext from "./Context";
 import axios from 'axios';
 
 function GlobalProvider({children}: any) {
-  const [ issues, setIssues ] = useState([]);
+
+  const url = 'http://localhost:3002'
+
+  interface IIssues {
+    id?: string;
+    number: number;
+    date: Date;
+    cover: string;
+    file: string;
+    language: string;
+    topics: string
+  }
+
+  const [ issues, setIssues ] = useState<IIssues[] | undefined>([]);
+
+  const getData = async (url: string) => {
+    try {
+        const response = await axios.get(`${url}/issues`);
+        const results = response.data;
+        setIssues(results)
+        console.log(issues);
+    } catch (error: Error | any) {
+        console.log(`${error.message}`);
+    }
+  }
+  
+  useEffect(() => {
+    getData(url)
+  }, [])
+  
   const context = {
+    url,
     issues, 
     setIssues
   }
--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -1,15 +1,35 @@
-import { Flex } from "@chakra-ui/react"
+import { Flex, Text, Button } from "@chakra-ui/react"
 import ContainerLayout from "../components/Layouts/Container";
+import { useContext } from "react";
+import { GlobalContext } from "../context/Context";
+
+interface IIssues {
+    id?: string;
+    number: number;
+    date: Date;
+    cover: string;
+    file: string;
+    language: string;
+    topics: string
+}
 
 function CatalogPage() {
+    const { issues } = useContext<any>(GlobalContext)
+
     return (
-        <ContainerLayout>
-            <Flex
-            bgColor={'zinc'}
-            color={'mustard'}>
-                Página de catálogo
-            </Flex>
-        </ContainerLayout>
+        <Flex
+        m={{base: '10px', md: '70px', lg: '140px'}}
+        direction={'column'}
+        gap={'37px'}>
+            <ContainerLayout>
+                
+                {issues && issues.map((issue: IIssues) => {
+                    return (
+                        <Text>{issue.topics}</Text>
+                    )
+                })}
+            </ContainerLayout>
+        </Flex>
     );
 }
 
